81c4c12c55106112a510d7438b391af7c3663990
Giafal <gianluca.falcone@yahoo.it>
2023-07-31 17:11:29 +0200

    esercizio g1

--- S6-G1/src/main/java/com/epicode/controller/WebPageController.java
+++ S6-G1/src/main/java/com/epicode/controller/WebPageController.java
@@ -1,10 +1,18 @@
 package com.epicode.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.epicode.model.Contatto;
+import com.epicode.services.ContattoService;
 
 @Controller
 public class WebPageController {
+	
+	@Autowired ContattoService cs;
 	
 	@GetMapping("/home")
 	public String getHomepage() {
@@ -19,6 +27,16 @@ public class WebPageController {
 	@GetMapping("/rubrica")
 	public String getRubrica() {
 		return "rubrica";
+	}
+	
+	@GetMapping("/rubricadettaglio/{id}")
+	public ModelAndView getRubricaById(@PathVariable Long id) {
+		Contatto contatto = cs.findById(id);
+		ModelAndView model = new ModelAndView("rubricadettaglio");
+		model.addObject("nome", contatto.getNome());
+		model.addObject("cognome", contatto.getCognome());
+		model.addObject("numero", contatto.getNumeroTelefono());
+		return model;
 	}
 
 }
