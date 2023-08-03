db0f7f6d3c1ecb02036263ce8012378ad2228a1a
Giafal <gianluca.falcone@yahoo.it>
2023-08-03 16:47:17 +0200

    esercizio g4

---
+++ S6-G4/src/main/java/com/epicode/S6G4Application.java
@@ -0,0 +1,13 @@
+package com.epicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S6G4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S6G4Application.class, args);
+	}
+
+}
